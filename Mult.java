e6533061e59f0822b3ca6b96cf1b2692f6770a07
pvwat4er <wat4er@gmail.com>
2017-12-01 00:27:44 +0700

    Initial commit

---
+++ Mult.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+import java.util.Map;
+
+public class Mult extends Command{
+
+    private Stack<Double> stack;
+
+    public void execute(Stack<Double> stack, Map<String, Double> define){
+
+        double x = stack.pop();
+        double y = stack.pop();
+        double z = x * y;
+        stack.push(z);
+
+
+    };
+
+}
